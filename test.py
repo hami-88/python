c535d576289a4b6dd108a59b5ba3a017b58c6344
hamed namvar <hamed.namvar@live.com>
2021-08-13 02:50:25 +0430

    v1.002

--- test.py
+++ test.py
@@ -27,7 +27,6 @@ price = input('how much did you pay?')
 price = float(price)
 if price >= 1.00:
     tax = .07
-    print('tax rate is: ' + str(tax))
 else:
         tax = 0
-        print('tax rate is: ' + str(tax))
+print('tax rate is: ' + str(tax))
