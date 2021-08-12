39e5f3fa2bd677973fa5c51763efd405912ab513
hamed namvar <hamed.namvar@live.com>
2021-08-13 01:30:23 +0430

    testi

--- test.py
+++ test.py
@@ -10,6 +10,13 @@ print(str(days)+' oridibehesht')
 from datetime import datetime, timedelta
 current_date = datetime.now()
 print('today is: ' + str(current_date))
+print('day: ' + str(current_date.day))
+print('month: ' + str(current_date.month))
+print('year: ' + str(current_date.year))
 one_day = timedelta(days = 1)
 yesterday = current_date - one_day
 print('yesterday : ' + str(yesterday))
+
+birthday = input('your birthday (dd/mm/yyyy)')
+birthday_date = datetime.strptime(birthday, '%d/%m/%Y')
+print('birthday: ' + str(birthday_date))
